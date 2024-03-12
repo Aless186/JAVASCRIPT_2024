8fa1a45d0a0f8e529164115ab22e862533469bdd
Alessandro Ninkovic <alessandro.ninkovic@gmail.com>
2024-03-12 19:09:27 +0100

    Add const, let & var

---
+++ 01-start/script.js
@@ -0,0 +1,17 @@
+// varijable možemo definirati s const, let i var
+
+// const način definiranja varijable se mora prilikom deklaracije odmah i inicijalizirati i ne mže se mijenjati vrijednost.
+
+const js = "JS je suuuuuper"
+
+// let način definiranja varijable se može deklarirat, naknadno inicijalizirati i može se mijenjati vrijednost.
+
+let js2;
+js2 = "JS je super!"
+
+var  varX= 5;
+var varX = 6;
+
+console.log (js);
+console.log (js2, varX);
+
