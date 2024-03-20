1a7c7383e757781af9825714911b0208bceed1a7
Alessandro Ninkovic <alessandro.ninkovic@gmail.com>
2024-03-20 18:35:17 +0100

    Add ternary operator

--- 07-funkcije/script.js
+++ 07-funkcije/script.js
@@ -1,1 +1,22 @@
-// 2 + 2
+// 2 + 2 je npr. expression jer će proizvesti novu vrijednost
+// 2000 je također expression jer je to određena vrijednost
+// if else i switch statement (deklaracije)
+//deklaracije možete gledati kao nekakve kompletne rečenice, expressioni bi bili riječi koje čine tu rečenicu
+// Statementi (deklaracije) su u biti sekvence akcija
+
+const godine = 23;
+
+if (godine >= 18) {
+    console.log("Punoljetan si");
+} else {
+    console.log("Maloljetan si");
+}
+
+// Trojni operator (ternary operator) možete gledati kao drugačiji način pisanja if else deklaracije.
+
+godine >= 18 ? console.log("Može") : console.log("Ne može");
+
+const Punoljetan = godine >= 18 ? "Može" : "Ne može";
+console.log(Punoljetan);
+
+console.log(`${godine >= 18 ? "može" : "ne može"}`);
