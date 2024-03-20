54124aa5e0367b34445862c616d0d91c2a156ab8
Alessandro Ninkovic <alessandro.ninkovic@gmail.com>
2024-03-20 19:04:45 +0100

    Add function

---
+++ 09-funkcije/script.js
@@ -0,0 +1,10 @@
+"use strict";
+
+// na ovaj način deklariramo funkciju i njen kod koji se izvršava kada se funkcija pokrene
+function ispis() {
+    console.log("Zovem se Alessandro");
+}
+
+// na ovaj način inicijaliziramo funkciju (calling, running, executing)
+ispis();
+ispis();
