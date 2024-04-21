5a8a5b2cce40fb8ce4dcbe7761252369340b19d0
Alessandro Ninkovic <alessandro.ninkovic@gmail.com>
2024-04-21 11:21:33 +0200

    Add callback hell resolution with Promise

--- 35/script.js
+++ 35/script.js
@@ -1,6 +1,10 @@
 "use strict";
 
-function getData(putanja, cb) {
+/* 
+REcimo da želimo napraviti više asinkronih poziva jer imamo podatke koji dolze iz različitih izvora i koji se moraju čitati u ispravnom redoslijedu. To bi mogli napraviti koristeći callback funkcije, međutim velika količina takvih poziva rezultira callback hellom iliti ovako ugnježđenim pozivima funkcija koji se onda budu zahvaljujući tom ugnježđenju izvodili u redoslijedu kako bi mi htjeli.
+*/
+
+/* function getData(putanja, cb) {
     const xhr = new XMLHttpRequest();
 
     xhr.open("GET", putanja);
@@ -27,6 +31,46 @@ getData("./filmovi.json", (filmovi) => {
         });
     });
 });
+ */
 
+/* _______________________________________________________ */
 
+/* S obzirom da je callback hell bio problem dugo vremena, uvođenje Promise rješenja je malo poboljšalo priču... */
 
+function getData(putanja) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+
+    xhr.open("GET", putanja);
+
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.response));
+        } else {
+          reject("Dogodila se greška...");
+        }
+      }
+    };
+
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 2000) + 100);
+  });
+}
+
+getData("./filmovi.json")
+  .then((filmovi) => {
+    console.log(filmovi);
+    return getData("./glumci.json");
+  })
+  .then((glumci) => {
+    console.log(glumci);
+    return getData("./redatelji.json");
+  })
+  .then((redatelji) => {
+    console.log(redatelji);
+  })
+  .catch((greska) => {
+    console.log(greska);
+  });
